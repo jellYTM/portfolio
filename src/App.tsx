b7cc9963780c0598d225730efe08f5e2373cd5bb
jellYTM <t.masaki1226@outlook.jp>
2025-04-18 19:42:41 +0900

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ function App() {
 
   return (
     <>
-    aaaaaa
-      {/* <div>
+      <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Staurodiscus_gotoi</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p> */}
+      </p>
     </>
   )
 }
