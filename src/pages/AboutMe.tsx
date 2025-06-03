921fc79f8fbd7cfebad5c42980476e97621e00e1
jellYTM <t.masaki1226@outlook.jp>
2025-06-03 17:46:29 +0900

    fix Base-and-img and add moreinfo

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -2,18 +2,35 @@ import Me1 from '../assets/Me1.jpg'
 
 function AboutMe() {
     return (
-        <div className='container'> 
+        <div> 
             <h1>About Me</h1>
-            <div id="Base-and-img">
-                <img src={Me1} />
-                <div id="BaseInfo">
-                    <h2>Base Info</h2>
-                    <p><span>氏名</span>： 竹内 優輝 （Masaki Takeuchi）</p>
-                    <p><span>性別</span>： 男</p>
-                    <p><span>誕生日</span>： 2004年 12月 1日</p>
-                    <p><span>所属</span>： 名古屋大学 理学部 生命理学科</p>
-                    <p><span>学年</span>： 学部 2年</p>
+            <div className='container'>
+                <h2>Base Info</h2>
+                <div id="Base-and-img">
+                    <img src={Me1} />
+                    <div id="BaseInfo">
+                        <p><span>氏名</span>： 竹内 優輝 （Masaki Takeuchi）</p>
+                        <p><span>性別</span>： 男</p>
+                        <p><span>誕生日</span>： 2004年 12月 1日 （20歳）</p>
+                        <p><span>所属</span>： 名古屋大学 理学部 生命理学科</p>
+                        <p><span>学年</span>： 学部 2年</p>
+                    </div>
                 </div>
+            </div>
+            <h2>More Info</h2>
+            <div className='wrap-flexbox'>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
+                <div>ボタン１</div>
             </div>
         </div>
     )
--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -4,8 +4,8 @@ function Footer() {
             <h3>お問い合わせ</h3>
             <ul>
                 <li>Email: <a href="t.masaki1226@outlook.jp">t.masaki1226@outlook.jp</a></li>
-                <li><a href="https://x.com/nj8Jr31dRi4mqy8">X(旧 twitter)</a></li>
-                <li><a href="https://www.instagram.com/tj.masaki/?igsh=MWc5OW9sNTV3cGF4Nw%3D%3D#">Instagram</a></li>
+                <li>X（旧 twitter）：<a href="https://x.com/nj8Jr31dRi4mqy8">@nj8Jr31dRi4mqy8</a></li>
+                <li>Instagram<a href="https://www.instagram.com/tj.masaki/?igsh=MWc5OW9sNTV3cGF4Nw%3D%3D#">tj.masaki</a></li>
             </ul>
         </div>
     )
